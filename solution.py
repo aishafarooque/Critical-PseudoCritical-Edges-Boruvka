de70f9575390b96eb65721e0ae3807be5db268e2
Aisha Farooque <aishafarooque@users.noreply.github.com>
2022-10-25 12:25:31 -0500

    formatted to meet leetcode requirements

--- solution.py
+++ solution.py
@@ -5,11 +5,11 @@ class Graph():
 
         self.vertices = set()
         self.totalWeight = 0
-    
+
     def add(self, u, v, weight):
         self.graph[u][v] = weight
         self.graph[v][u] = weight
-        
+
         self.vertices.add(u)
         self.vertices.add(v)
 
@@ -33,11 +33,11 @@ class Graph():
         edges = []
 
         for i in range(0, len(self.graph)):
-            edges.append(self.getEdge(u,i))
-        
+            edges.append(self.getEdge(u, i))
+
         # print (f'Getting {u} edges: {edges}')
         return edges
-    
+
     def deleteEdge(self, u, v):
         try:
             edge = self.graph[u][v]
@@ -48,7 +48,7 @@ class Graph():
                 return edge
         except:
             return None
-    
+
     def getNumberOfVertices(self):
         return len(self.vertices)
 
@@ -61,33 +61,35 @@ class Graph():
             Vertex #3: 0: 2, 2: 2, 4: 3
             Vertex #4: 0: 3, 1: 6, 3: 3
         '''
-        for i in range(0, len(self.graph)):
-            res = ''
-            for j in range(0, len(self.graph[i])):
-                if self.graph[i][j] != -1:
-                    res += (f'{j}: {self.graph[i][j]}, ')
-            print (f'Vertex #{i}: {res}'[:-2])
-        print ()
-    
+        # for i in range(0, len(self.graph)):
+        #     res = ''
+        #     for j in range(0, len(self.graph[i])):
+        #         if self.graph[i][j] != -1:
+        #             res += (f'{j}: {self.graph[i][j]}, ')
+        #     print (f'Vertex #{i}: {res}'[:-2])
+        # print ()
+
     def printGraphAsTable(self):
-        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in self.graph]))
+        print('\n'.join([''.join(['{:4}'.format(item)
+              for item in row]) for row in self.graph]))
 
     def __len__(self):
         return self.__len__
 
+
 class ConnectedComponents():
-    def __init__(self, numberOfEdges) -> None:
+    def __init__(self, numberOfEdges):
         self.n = numberOfEdges
         self.nodes = []
         for i in range(0, numberOfEdges):
-            self.nodes.append({ i })
+            self.nodes.append({i})
 
     def find(self, u):
-        for i in range(0, n):
+        for i in range(0, self.n-1):
             if u in self.nodes[i]:
                 return i
         return -1
-    
+
     def connect(self, u, v):
         '''
         Merges u and v together. For example:
@@ -98,25 +100,25 @@ class ConnectedComponents():
         Remaining nodes after merging are: [{0, 1, 2, 3}, {4}]
         Remaining nodes after merging are: [{0, 1, 2, 3, 4}]
         '''
-        
+
         indexU = self.find(u)
         indexV = self.find(v)
 
-        if indexU == -1 or indexV == -1 or indexU == indexV: 
-            print (f'Ineligible conditions, u = {u}, v = {v}')
+        if indexU == -1 or indexV == -1 or indexU == indexV:
+            print ('Ineligible conditions, u = ', u, ' v = ', v)
             return
-        
+
         self.nodes[indexU].update(self.nodes[indexV])
         self.nodes.pop(indexV)
-        
-        # print (f'Merged {u} and {v}')
-        # print (f'Remaining nodes after merging are: {self.nodes}')
-    
+
+        # print ('Merged ', u , ' and ', v)
+        # print ('Remaining nodes after merging are: ', self.nodes)
+
     def getMinimumWeightedEdges(self, graph, debug=False):
         minimumEdge = None
         for node in self.nodes:
             for vertex in node:
-                edges = graph.getEdges(vertex) 
+                edges = graph.getEdges(vertex)
                 # (print (f'node = {node}, vertex = {vertex}, edges = {edges}') if debug else None)
                 for i in range(0, len(edges)-1):
                     if edges[i] != -1:
@@ -127,96 +129,102 @@ class ConnectedComponents():
                                 minimumEdge = (source, target, weight)
                                 # print (f'node = {node}, vertex = {vertex}, edges = {edges}')
                                 # print (f'Minimum edge weight is graph[{minimumEdge[0]}][{minimumEdge[1]}]={minimumEdge[2]}')
-        (print (f'Final minimum edge is {minimumEdge}') if debug else None)
+        # (print ('Final minimum edge is ', minimumEdge) if debug else None)
         return minimumEdge
 
-    def print(self):
-        print(self.nodes)
+class Solution():
+    def boruvka(self, graph, debug=False):
+        """
+        Computes MST using Boruvska's algorithm.
+        Assumes all edges are distinct. 
 
-def boruvka(graph, debug=False):
-    """
-    Computes MST using Boruvska's algorithm.
-    Assumes all edges are distinct. 
+        Parameters
+        ----------
+        n : int
+            number of vertices 
+        edges : list[list]
+            weighted undirected connected graph
+        """
 
-    Parameters
-    ----------
-    n : int
-        number of vertices 
-    edges : list[list]
-        weighted undirected connected graph
-    """
+        numberOfEdges = graph.getNumberOfVertices()
 
-    numberOfEdges = graph.getNumberOfVertices()
+        # Initialize a forest F to (V, E') where E' = {}.
+        connectedComponents = ConnectedComponents(numberOfEdges)
 
-    # Initialize a forest F to (V, E') where E' = {}.
-    connectedComponents = ConnectedComponents(numberOfEdges)
+        # Stores the resulting minimum spanning tree
+        minimumSpanningTree = Graph(numberOfEdges)
 
-    # Stores the resulting minimum spanning tree
-    minimumSpanningTree = Graph(numberOfEdges)
+        # Iterate until there are independently connected components
+        while len(connectedComponents.nodes) > 1:
 
-    # Iterate until there are independently connected components
-    while len(connectedComponents.nodes) > 1:
+            try:
+                # Select the minimum-weight edge
+                minimumEdgeWeightSource, minimumEdgeWeightTarget, minimumEdgeWeight = \
+                    connectedComponents.getMinimumWeightedEdges(graph, debug)
 
-        try:
-            # Select the minimum-weight edge
-            minimumEdgeWeightSource, minimumEdgeWeightTarget, minimumEdgeWeight = \
-                connectedComponents.getMinimumWeightedEdges(graph, debug)
-            
-            # Edge Contraction: an edge is chosen from the graph and a new node is formed with the union 
-            # of the connectivity of the incident nodes of the chosen edge.
-            # References: https://iss.oden.utexas.edu/?p=projects/galois/analytics/mst
-            connectedComponents.connect(minimumEdgeWeightSource, minimumEdgeWeightTarget)
+                # Edge Contraction: an edge is chosen from the graph and a new node is formed with the union
+                # of the connectivity of the incident nodes of the chosen edge.
+                # References: https://iss.oden.utexas.edu/?p=projects/galois/analytics/mst
+                connectedComponents.connect(
+                    minimumEdgeWeightSource, minimumEdgeWeightTarget)
 
-            # Eliminate all but the lowest-weight edge among each set of multiple edges
-            minimumSpanningTree.add(minimumEdgeWeightSource, minimumEdgeWeightTarget, minimumEdgeWeight)
-        except:
-            return -1
-    
-    # Return the weight of the MST 
-    return minimumSpanningTree.totalWeight
+                # Eliminate all but the lowest-weight edge among each set of multiple edges
+                minimumSpanningTree.add(
+                    minimumEdgeWeightSource, minimumEdgeWeightTarget, minimumEdgeWeight)
+            except:
+                return -1
 
-def driver(n, edges, tests):
-    debug = False
-    debugIndex = -1
+        # Return the weight of the MST
+        return minimumSpanningTree.totalWeight
 
-    graph = Graph(n)
-    
-    # Build the graph
-    for i in range(0, len(edges)):
-        source, target, weight = edges[i]
-        graph.add(source, target, weight)
 
-    criticalEdge = []
-    pseudoCriticalEdge = []
-    mstAllEdges = boruvka(graph, debug)
-    if tests: assert(mstAllEdges == 7)
+    def findCriticalAndPseudoCriticalEdges(self, n, edges, tests=None):
+        debug = False
+        debugIndex = -1
 
-    for i in range(0, len(edges)):
-        edge = edges[i]
-        source, target, weight = edge
-        graph.deleteEdge(source, target)
+        graph = Graph(n)
 
-        mstWithoutOneEdge = boruvka(graph, debug=(True if i == debugIndex else False))
+        # Build the graph
+        for i in range(0, len(edges)):
+            source, target, weight = edges[i]
+            graph.add(source, target, weight)
 
-        if tests: assert(mstWithoutOneEdge == tests[i])
+        criticalEdge = []
+        pseudoCriticalEdge = []
+        mstAllEdges = self.boruvka(graph, debug)
+        if tests:
+            assert(mstAllEdges == 7)
 
-        if mstWithoutOneEdge > mstAllEdges or mstWithoutOneEdge == -1:
-            criticalEdge.append(i)
-        elif mstWithoutOneEdge == mstAllEdges:
-            pseudoCriticalEdge.append(i)
-        
-        graph.add(source, target, weight)
+        for i in range(0, len(edges)):
+            edge = edges[i]
+            source, target, weight = edge
+            graph.deleteEdge(source, target)
 
-    return [criticalEdge, pseudoCriticalEdge]
+            mstWithoutOneEdge = self.boruvka(graph, debug=(
+                True if i == debugIndex else False))
+
+            if tests:
+                assert(mstWithoutOneEdge == tests[i])
+
+            if mstWithoutOneEdge > mstAllEdges or mstWithoutOneEdge == -1:
+                criticalEdge.append(i)
+            elif mstWithoutOneEdge == mstAllEdges:
+                pseudoCriticalEdge.append(i)
+
+            graph.add(source, target, weight)
+
+        return [criticalEdge, pseudoCriticalEdge]
+
 
 if __name__ == "__main__":
-    edges = [[0,1,1],[1,2,1],[2,3,2],[0,3,2],[0,4,3],[3,4,3],[1,4,6]]
-    tests = [8,8,7,7,7,7,7]
+    solution = Solution()
+    edges = [[0, 1, 1], [1, 2, 1], [2, 3, 2], [0, 3, 2], [0, 4, 3], [3, 4, 3], [1, 4, 6]]
+    tests = [8, 8, 7, 7, 7, 7, 7]
     n = 5
-    mst = driver(n, edges, tests)
-    # print (mst)
-
-    edges = [[0,1,1],[1,2,1],[2,3,1],[0,3,1]]
-    n = 4
-    mst = driver(n, edges, tests)
+    mst = solution.findCriticalAndPseudoCriticalEdges(n, edges, tests)
     print (mst)
+
+    # edges = [[0, 1, 1], [1, 2, 1], [2, 3, 1], [0, 3, 1]]
+    # n = 4
+    # mst = solution.findCriticalAndPseudoCriticalEdges(n, edges, None)
+    # print(mst)
